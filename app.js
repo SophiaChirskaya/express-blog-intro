8941e985b47c2e55da6f6f5873cc9b3aaca1b1af
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-02-04 20:27:52 +0100

    final vs

--- app.js
+++ app.js
@@ -16,31 +16,31 @@ app.get('/blog', (req, res) => {
         {
             titolo: "Pane Fresco",
             contenuti: "Pane fatto in casa con farina di grano tenero e lievito naturale.",
-            immagine: "http://localhost:3000/img/ciambellone.webp",
+            immagine: "http://localhost:3000/img/ciambellone.jpeg",
             tags: ["pane", "fresco", "artigianale", "lievito naturale"]
           },
           {
             titolo: "Cornetto al Burro",
             contenuti: "Cornetto soffice e burroso, perfetto per la colazione.",
-            immagine: "http://localhost:3000/img/cracker_barbabietola.webp",
+            immagine: "http://localhost:3000/img/cracker_barbabietola.jpeg",
             tags: ["cornetto", "colazione", "burro", "dolce"]
           },
           {
             titolo: "Pizza Margherita",
             contenuti: "Pizza con pomodoro fresco, mozzarella di bufala e basilico.",
-            immagine: "http://localhost:3000/img/pane_fritto_dolce.webp",
+            immagine: "http://localhost:3000/img/pane_fritto_dolce.jpeg",
             tags: ["pizza", "margherita", "pomodoro", "mozzarella"]
           },
           {
             titolo: "Focaccia Genovese",
             contenuti: "Focaccia morbida e fragrante, con rosmarino e sale grosso.",
-            immagine: "http://localhost:3000/img/pasta_barbabietola.webp",
+            immagine: "http://localhost:3000/img/pasta_barbabietola.jpeg",
             tags: ["focaccia", "genovese", "rosmarino", "sale grosso"]
           },
           {
             titolo: "Brioche Siciliana",
             contenuti: "Brioche dolce, soffice e perfetta per accompagnare granite siciliane.",
-            immagine: "http://localhost:3000/img/torta_paesana.webp",
+            immagine: "http://localhost:3000/img/torta_paesana.jpeg",
             tags: ["brioche", "siciliana", "dolce", "granita"]
           }
     ];

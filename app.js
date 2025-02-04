88d505c68c34be977e789d5280285fc2375e2bae
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-02-04 20:17:14 +0100

    [feat]  response in json

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-// Definisco l'uso di una cartella per i file statici
+// Definizione d'uso di una cartella per i file statici
 app.use(express.static('public'));
 
 // Definizone di rotta home
@@ -12,38 +12,40 @@ app.get('/', (req, res) => {
 
 // Definizione rotta del blog
 app.get('/blog', (req, res) => {
-    const blog =[
+    const blog = [
         {
             titolo: "Pane Fresco",
             contenuti: "Pane fatto in casa con farina di grano tenero e lievito naturale.",
-            immagine: "https://example.com/immagini/pane_fresco.jpg",
+            immagine: "http://localhost:3000//ciambellone.jpeg",
             tags: ["pane", "fresco", "artigianale", "lievito naturale"]
           },
           {
             titolo: "Cornetto al Burro",
             contenuti: "Cornetto soffice e burroso, perfetto per la colazione.",
-            immagine: "https://example.com/immagini/cornetto_burro.jpg",
+            immagine: "http://localhost:3000/cracker_barbabietola.jpeg",
             tags: ["cornetto", "colazione", "burro", "dolce"]
           },
           {
             titolo: "Pizza Margherita",
             contenuti: "Pizza con pomodoro fresco, mozzarella di bufala e basilico.",
-            immagine: "https://example.com/immagini/pizza_margherita.jpg",
+            immagine: "img/pane_fritto_dolce.jpeg",
             tags: ["pizza", "margherita", "pomodoro", "mozzarella"]
           },
           {
             titolo: "Focaccia Genovese",
             contenuti: "Focaccia morbida e fragrante, con rosmarino e sale grosso.",
-            immagine: "https://example.com/immagini/focaccia_genovese.jpg",
+            immagine: "img/pasta_barbabietola.jpeg",
             tags: ["focaccia", "genovese", "rosmarino", "sale grosso"]
           },
           {
             titolo: "Brioche Siciliana",
             contenuti: "Brioche dolce, soffice e perfetta per accompagnare granite siciliane.",
-            immagine: "https://example.com/immagini/brioche_siciliana.jpg",
+            immagine: "img/torta_paesana.jpeg",
             tags: ["brioche", "siciliana", "dolce", "granita"]
           }
-    ]
+    ];
+    // array in JSON da ritornare per l'endpoint dell'API
+    res.json(blog);
 })
 
 app.listen(port, () => {
